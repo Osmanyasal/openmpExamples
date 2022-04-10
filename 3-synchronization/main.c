4176a26cd61cc02dffb57d7f11f417b94f46eb50
osman yasal <osmanyasal@192.168.1.2>
2022-04-10 12:18:15 +0300

    play around

--- 3-synchronization/main.c
+++ 3-synchronization/main.c
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 #include <omp.h>
 
-int main(int argc,char *argv[]){
-    
-    omp_set_num_threads(8);
-    #pragma omp parallel
+int main(int argc, char *argv[])
+{
+    int sh = 1;
+
+    omp_set_num_threads(3);
+    #pragma omp parallel shared(sh)
     {
-        #pragma omp single nowait
+        printf("TH %d\tsh %d\n",omp_get_thread_num(),sh);
+        #pragma omp single
         {
-            printf("this is thread [%d] and we created %d threads\n",omp_get_thread_num(),omp_get_num_threads());
+            sh++; 
+            printf("------\n");
         }
-        printf("is %d okay?\n",omp_get_thread_num());
+        printf("AFTER TH %d\tsh %d\n",omp_get_thread_num(),sh);        
     }
 }
--- 4-matrixOperations/macros/ompwrapper.h
+++ 4-matrixOperations/macros/ompwrapper.h
@@ -4,4 +4,5 @@
 #else
     #define omp_get_thread_num() 0
     #define omp_get_num_threads() 1
+    #define omp_in_parallel() 0
 #endif
